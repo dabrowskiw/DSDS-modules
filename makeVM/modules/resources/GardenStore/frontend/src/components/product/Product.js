ed0420e8faf272728cec626ac4fda28fc4c0bdbe
butburg <fire.fm@hotmail.de>
2022-05-03 15:21:52 +0200

    add moduke product

---
+++ makeVM/modules/resources/GardenStore/frontend/src/components/product/Product.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import '../styles.css';
+import { useNavigate } from 'react-router';
+import Moment from 'moment';
+
+
+const Product = ({
+    product_id,
+    name,
+    description,
+    img,
+    comments,
+    rates,
+    amount
+}) => {
+    function formatDate(date) {
+        return Moment.utc(date).format("DD.MM.YYYY");
+    }
+
+    return (
+        <Card>
+            <Card.Body>
+                <div className="trip-details">
+                    <div>{name} </div>
+                    <div>{img} </div>
+                    <div>{description} </div>
+                    <div>{amount} </div>
+                    <div>{rates} </div>
+                </div>
+
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default Product;
