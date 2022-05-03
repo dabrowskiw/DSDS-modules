9f78d46383d51fc2a689f88c1d4bf8e70e39847c
pomcom <pomcom@mailbox.org>
2022-05-03 19:48:25 +0200

    add config copy - cleanup installation process

--- makeVM/modules/docker-security-flaws.sh
+++ makeVM/modules/docker-security-flaws.sh
@@ -4,8 +4,12 @@ vboxmanage startvm "${VMNAME}" --type headless
 echo "Waiting for VM to come up..."
 sleep 8
 
+echo "Copying setup.sh to vm"
+echo ""
 scp -P 2200 -i temp/rootkey -r modules/resources/Docker_Security_Flaws/setup.sh root@127.0.0.1:/root/
+
+echo "Running docker-security-flaws setup"
+echo ""
 ssh -p 2200 -i ${TMPDIR}/rootkey root@127.0.0.1 "bash /root/setup.sh"
 
 
-scp -r -P 2200 -i temp/rootkey -r modules/resources/Docker_Security_Flaws/config/daemon.js root@127.0.0.1:/etc/docker/
--- makeVM/modules/resources/Docker_Security_Flaws/setup.sh
+++ makeVM/modules/resources/Docker_Security_Flaws/setup.sh
@@ -1,28 +1,49 @@
 #!/usr/bin/env bash
 
+set -u
+set -e
+set -o pipefail 
 
 # curl just now needed - delete later on
+echo "Installing dependencies"
+echo ""
 apt-get -y update && apt-get -y install docker.io && apt-get -y install curl
 
 # could be deleted later on
+echo "Verifying docker is working"
+echo ""
 docker run hello-world
 
+
+echo "Start docker registry on port 5000"
+echo ""
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 # can be deleted later on
+
 docker ps
 
 
+echo "Copying config for unsecure docker communication"
+echo ""
 scp -r -P 2200 -i temp/rootkey -r modules/resources/Docker_Security_Flaws/config/daemon.js root@127.0.0.1:/etc/docker/
 
-# pushing test image to registry
+echo "pushing test image to registry"
+echo ""
 docker tag hello-world 127.0.0.1:5000/hello-world
 docker push 127.0.0.1:5000/hello-world
+
+echo "restarting docker registry"
+echo ""
 docker restart registry
 
 # verifying local
+echo "Verifying registry ist running and image was pushed successfully"
+echo ""
 curl -X GET http://127.0.0.1:5000/v2/_catalog
 
+echo ""
+echo "Setup succesfull."
 
 
 
