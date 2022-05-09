036cc5430401d83b22baead10e1ff16982c93c0e
butburg <fire.fm@hotmail.de>
2022-05-10 01:06:55 +0200

    improve look

---
+++ makeVM/modules/resources/GardenStore/frontend/src/components/product/Detail.js
@@ -0,0 +1,144 @@
+import React, { useState, useEffect } from "react";
+import _ from "lodash";
+import Product from "./Product";
+import Header from "../structure/Header";
+import "../styles.css";
+import { useNavigate } from "react-router";
+import 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const List = (props) => {
+
+  const BASE_URL = "http://localhost:3001";
+
+  let navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [product, setProduct] = useState([]);
+
+  const path = window.location.pathname;
+
+  useEffect(() => {
+
+    // if (!props.logged) {
+    //   navigate("/");
+    // }
+
+    let mounted = true;
+
+    setTimeout(() => {
+      async function getProduct() {
+        /* TODO ersetze 1 mit entsprechender url */
+        fetch(`${BASE_URL}/products/1`, {
+          method: "GET",
+          credentials: "include",
+        })
+          .then((res) => res.json())
+          .then(
+            (result) => {
+              if (mounted) {
+                setIsLoaded(true);
+                setProduct(result);
+              }
+            },
+            (error) => {
+              if (mounted) {
+                setIsLoaded(true);
+                setError(error);
+              }
+            }
+          );
+      } getProduct();
+    }, 2000);
+    return () => (mounted = false); //cleanup function
+  }, [product, BASE_URL, navigate, path, props.logged]);
+
+  const logout = () => {
+    props.onLogout();
+  };
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div className="loading-screen">Loading</div>;
+  } else {
+    return (
+      <div>
+        <React.Fragment>
+          {/* <header>
+                <Header onLogout={logout} />
+            </header> */}
+          {/* <tr key={index}>
+                        <td>{product.name}</td>
+                        <td>{product.description}</td>
+                        <td>{product.amount}</td>
+                        <td>{product.rates}</td>
+                      </tr> */}
+          <section style={{ backgroundColor: '#eee' }}>
+            <div className="container py-5">
+              <div className="row justify-content-center mb-3">
+                {!_.isEmpty(product) ? (
+                    /* TODO get only one product without map? */
+                  product.map(
+                    (product, index) => {
+                      return (
+                        <div className="col-md-12 col-xl-10">
+                          <div className="card shadow-0 border rounded-3">
+                            <div className="card-body">
+                              <div className="row">
+                                <div className="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
+                                  <div className="bg-image hover-zoom ripple rounded ripple-surface">
+                                    <img src="http://oh-eweedy.bplaced.net/Bilder/funaSmall.JPG" className="w-100" />
+                                    <a href="#!">
+                                      <div className="hover-overlay">
+                                        <div className="mask" style={{ backgroundColor: 'rgba(253, 253, 253, 0.15)' }} />
+                                      </div>
+                                    </a>
+                                  </div>
+                                </div>
+                                <div className="col-md-6 col-lg-6 col-xl-6">
+                                  <h5 className="text-center">{product.name}</h5>
+
+                                  <p className="text-right">{product.rates} Sterne</p>
+
+
+                                  <p className="mb-4 mb-md-0">
+                                    {product.description}
+                                  </p>
+                                </div>
+                                <div className="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
+                                  <div className="d-flex flex-row align-items-center mb-1">
+                                    <h4 className="mb-1 me-1">${product.price}</h4>
+
+                                    <h6 className="text-success">{product.amount} in stock</h6>
+                                  </div>
+
+                                  <div className="d-flex flex-column mt-4">
+                                    <button className="btn btn-primary btn-sm" type="button"></button>
+                                    <button className="btn btn-outline-primary btn-sm mt-2" type="button">
+                                      Add to Cart
+                                    </button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+
+                      )
+                    })
+                ) : (
+                  <p className="message">Empty</p>
+                )}
+              </div>
+            </div>
+          </section>
+
+        </React.Fragment>
+      </div>
+    );
+  };
+};
+
+export default List;
--- makeVM/modules/resources/GardenStore/frontend/src/components/product/List.js
+++ makeVM/modules/resources/GardenStore/frontend/src/components/product/List.js
@@ -75,11 +75,11 @@ const List = (props) => {
                       </tr> */}
           <section style={{ backgroundColor: '#eee' }}>
             <div className="container py-5">
-            <div className="row justify-content-center mb-3">
-              {!_.isEmpty(products) ? (
-                products.map(
-                  (product, index) => {
-                    return (
+              <div className="row justify-content-center mb-3">
+                {!_.isEmpty(products) ? (
+                  products.map(
+                    (product, index) => {
+                      return (
                         <div className="col-md-12 col-xl-10">
                           <div className="card shadow-0 border rounded-3">
                             <div className="card-body">
@@ -95,44 +95,26 @@ const List = (props) => {
                                   </div>
                                 </div>
                                 <div className="col-md-6 col-lg-6 col-xl-6">
-                                  <h5>{product.name}</h5>
-                                  <div className="d-flex flex-row">
-                                    <div className="text-danger mb-1 me-2">
-                                      <i className="fa fa-star" />
-                                      <i className="fa fa-star" />
-                                      <i className="fa fa-star" />
-                                      <i className="fa fa-star" />
-                                    </div>
-                                    <span>{product.rates} Sterne</span>
-                                  </div>
-                                  <div className="mt-1 mb-0 text-muted small">
-                                    <span>100% cotton</span>
-                                    <span className="text-primary"> • </span>
-                                    <span>Light weight</span>
-                                    <span className="text-primary"> • </span>
-                                    <span>Best finish<br /></span>
-                                  </div>
-                                  <div className="mb-2 text-muted small">
-                                    <span>Unique design</span>
-                                    <span className="text-primary"> • </span>
-                                    <span>For men</span>
-                                    <span className="text-primary"> • </span>
-                                    <span>Casual<br /></span>
-                                  </div>
-                                  <p className="text-truncate mb-4 mb-md-0">
+                                  <h5 className="text-center">{product.name}</h5>
+
+                                  <p className="text-right">{product.rates} Sterne</p>
+
+
+                                  <p className="mb-4 mb-md-0">
                                     {product.description}
                                   </p>
                                 </div>
                                 <div className="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
                                   <div className="d-flex flex-row align-items-center mb-1">
                                     <h4 className="mb-1 me-1">${product.price}</h4>
-                                    <span className="text-danger"><s>$20.00</s></span>
+
+                                    <h6 className="text-success">{product.amount} in stock</h6>
                                   </div>
-                                  <h6 className="text-success">{product.amount} in stock</h6>
+
                                   <div className="d-flex flex-column mt-4">
                                     <button className="btn btn-primary btn-sm" type="button">Details</button>
                                     <button className="btn btn-outline-primary btn-sm mt-2" type="button">
-                                      Add to wishlist
+                                      Add to Cart
                                     </button>
                                   </div>
                                 </div>
@@ -140,14 +122,14 @@ const List = (props) => {
                             </div>
                           </div>
                         </div>
-                      
 
-                    )
-                  })
-              ) : (
-                <p className="message">Empty</p>
-              )}
-            </div>
+
+                      )
+                    })
+                ) : (
+                  <p className="message">Empty</p>
+                )}
+              </div>
             </div>
           </section>
 
