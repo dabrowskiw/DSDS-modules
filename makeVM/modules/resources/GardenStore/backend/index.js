70e50bfde730cc653b58a982b76b3ddc4a44112f
simontree <simonroof@tutanota.com>
2022-05-24 17:15:46 +0200

    added login and user endpoint and cookie feature

--- makeVM/modules/resources/GardenStore/backend/index.js
+++ makeVM/modules/resources/GardenStore/backend/index.js
@@ -3,7 +3,10 @@
 // import { HttpError } from "express-openapi-validator/dist/framework/types";   
 
 const express = require("express");
-const pool = require("./db/database");
+const pool = require("./Database/database");
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
+
 require('dotenv').config();
 
 const app = express();
@@ -42,6 +45,7 @@ app.get("/comments/:id", async (req,res) => {
   try {
     const sqlQuery = 'SELECT comm_id, author, text, rating, created_at, product_id FROM comments WHERE product_id=?';
     const rows = await pool.query(sqlQuery, req.params.id);
+    // console.log(rows[0].author);
     res.status(200).send(rows);
   } catch (error) {
     res.status(400).send(error.message);
@@ -56,6 +60,66 @@ app.post("/comments", async (req,res) => {
     console.log(result);
     res.status(200);   // TODO: request result throws error that big int can't be parsed 
     return res.json({message: "comment created successfully"});
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+})
+/**
+ * Login endpoint
+ */
+
+app.post("/login", async (req,res) =>{
+  const payload = req.body;
+  const sessionId = await login(payload.email, payload.password);
+  // console.log(sessionId);
+  if(!sessionId){
+    res.status(401);  //401 -> unauthorized
+    return res.json({ message: "Wrong Email or Password."})
+  }
+  res.cookie("sessionCookie", sessionId,{
+    // httpOnly: true,  //uncomment to prevent XSS attack if browser accepts httpOnly header
+    sameSite: "none",  //none = sent cookies in all contexts, 1st party and XS requests
+    secure: true  //only allow secure sending of cookies - only encrypted req allowed
+  });
+  res.status(200);
+  return res.json({status:200, sessionId: sessionId});
+})
+
+async function login(email, password){
+  const isPasswordCorrect = await checkPw(email, password);
+  if(isPasswordCorrect){
+    const sessionId = crypto.randomUUID();
+    return sessionId;
+  }
+  return undefined;
+}
+
+// check salted & hashedPW with bcrypt
+async function checkPw(email, password){
+  try {
+    const sqlQuery = 'SELECT email, password FROM users WHERE email=?';
+    const rows = await pool.query(sqlQuery, email);
+    return bcrypt.compare(password, rows[0].password);
+  } catch (error) {
+    return undefined;
+  }
+}
+
+/**
+ * Users endpoint
+ */
+
+// creates user with salted password hash and adds it to db
+app.post("/users", async (req,res) => {
+  try {
+    const {username, plainpassword, iban, address, email} = req.body;
+    const salt = await bcrypt.genSalt();
+    const password = await bcrypt.hash(plainpassword, salt);
+    const sqlQuery = 'INSERT INTO users (username, password, iban, address, email) VALUES (?,?,?,?,?)';
+    const result = await pool.query(sqlQuery, [username, password, iban, address, email]);
+    console.log(result);
+    res.status(200);  
+    return res.json({message: "user created successfully"});
   } catch (error) {
     res.status(400).send(error.message);
   }
