045d3fe948ef88fefd6e238cbf5a5e38d5314b5a
simontree <simonroof@tutanota.com>
2022-08-11 19:22:20 +0200

    working on database setup on VM

--- makeVM/modules/resources/GardenStore/setup.sh
+++ makeVM/modules/resources/GardenStore/setup.sh
@@ -7,6 +7,22 @@ apt-get install curl -y
 curl -sL https://deb.nodesource.com/setup_18.x | bash -
 apt install nodejs
 
+# setup database 
+apt-get install mariadb-server
+
+cd backend/Database
+
+mysql
+create database sys;
+create user mario@localhost identified by 'Its4321?!';
+grant all privileges on sys.* to mario@localhost;
+exit
+mysql -p sys < create_tables.sql
+Its4321?!       # enter password to execute sql-file
+mysql -p sys < INSERT_products_users.sql
+Its4321?!       # enter password to execute sql-file
+mysql -p sys < INSERT_comments.sql
+Its4321?!       # enter password to execute sql-file
 
 cd backend
 #install dependencies backend
@@ -18,4 +34,4 @@ cd ../frontend
 #install dependencies frontend
 npm i
 #start frontend
-npm start
+npm run start
