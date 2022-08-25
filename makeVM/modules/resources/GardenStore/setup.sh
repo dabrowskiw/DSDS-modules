a58788e2e7326920e6e893e6596787ee6befab0f
simontree <simonroof@tutanota.com>
2022-08-25 16:48:32 +0200

    minor updates

--- makeVM/modules/resources/GardenStore/setup.sh
+++ makeVM/modules/resources/GardenStore/setup.sh
@@ -13,11 +13,13 @@ apt-get install -y nodejs
 # Setup Database Backend
 printf "\n--Setup database\n" 
 cd backend/Database
-mysql << EOF
+# mysql << EOF
 create database sys;
 create user mario@localhost identified by 'Its4321?!';
 grant all privileges on sys.* to mario@localhost;
 exit
+
+cd backend/Database
 mysql -p sys < create_tables.sql
 Its4321?!       # enter password to execute sql-file
 mysql -p sys < INSERT_products_users.sql
@@ -25,12 +27,13 @@ Its4321?!       # enter password to execute sql-file
 mysql -p sys < INSERT_comments.sql
 Its4321?!       # enter password to execute sql-file
 
-cd backend
+cd ../..
 #install dependencies backend
 npm i
 cd ../frontend
 npm i
-
+npm i react-scripts@5   # to solve "opensslerrorstack error 03000086 digital envelope init error"
+ 
 # Start Servers
 printf "\n--Start Server Backend\n"
 cd ../backend
