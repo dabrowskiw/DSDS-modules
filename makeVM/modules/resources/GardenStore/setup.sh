e7cbe916a5204d8d42705e790cb8703915f36487
Robert <59532700+simontree@users.noreply.github.com>
2022-09-02 16:15:39 +0200

    minor fixes

--- makeVM/modules/resources/GardenStore/setup.sh
+++ makeVM/modules/resources/GardenStore/setup.sh
@@ -9,34 +9,45 @@ apt-get install -y mariadb-server
 apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_18.x | bash -
 apt-get install -y nodejs
+apt-get install -y net-tools
 
 # Setup Database Backend
-printf "\n--Setup database\n" 
+printf "\n--Setup database...\n" 
 cd /home/mario/GardenStore/backend/Database
-mysql #<< EOF  # opens sub shell
+mysql << EOF  # opens sub shell
 create database sys;
 create user mario@localhost identified by 'Its4321?!';
 grant all privileges on sys.* to mario@localhost;
 exit
+EOF
 
 mysql -pIts4321?! sys < create_tables.sql
 mysql -pIts4321?! sys < INSERT_products_users.sql
 mysql -pIts4321?! sys < INSERT_comments.sql
 
+# Setup react
+printf "\n--Setup react\n" 
 cd /home/mario/GardenStore/backend
+npm install pm2 -g
 #install dependencies backend
 npm i
 cd /home/mario/GardenStore/frontend
 npm i
-npm i react-scripts@5   # to solve "opensslerrorstack error 03000086 digital envelope init error"
+
+
+#npm i react-scripts@5   # to solve "opensslerrorstack error 03000086 digital envelope init error"
  
 # Start Servers
 printf "\n--Start Server Backend\n"
 cd /home/mario/GardenStore/backend
-npm run start &
+pm2 --name backend start npm -- start
+
 
 printf "\n--Start Server Frontend\n"
 cd /home/mario/GardenStore/frontend
-npm run start &
+pm2 --name frontend start npm -- start
 
+
+pm2 ps
+read -p "Press any key to exit..."
 printf '\n--GardenStore is up.\n'
