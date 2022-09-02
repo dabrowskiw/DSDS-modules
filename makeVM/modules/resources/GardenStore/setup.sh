afb73f27ab5d1d13178ef5336ce1b1bd1cf8d427
Robert <59532700+simontree@users.noreply.github.com>
2022-09-02 12:10:19 +0200

    updated

--- makeVM/modules/resources/GardenStore/setup.sh
+++ makeVM/modules/resources/GardenStore/setup.sh
@@ -12,7 +12,7 @@ apt-get install -y nodejs
 
 # Setup Database Backend
 printf "\n--Setup database\n" 
-cd home/mario/GardenStore/backend/Database
+cd /home/mario/GardenStore/backend/Database
 mysql #<< EOF  # opens sub shell
 create database sys;
 create user mario@localhost identified by 'Its4321?!';
@@ -23,20 +23,20 @@ mysql -pIts4321?! sys < create_tables.sql
 mysql -pIts4321?! sys < INSERT_products_users.sql
 mysql -pIts4321?! sys < INSERT_comments.sql
 
-cd home/mario/GardenStore/backend
+cd /home/mario/GardenStore/backend
 #install dependencies backend
 npm i
-cd home/mario/GardenStore/frontend
+cd /home/mario/GardenStore/frontend
 npm i
 npm i react-scripts@5   # to solve "opensslerrorstack error 03000086 digital envelope init error"
  
 # Start Servers
 printf "\n--Start Server Backend\n"
-cd home/mario/GardenStore/backend
+cd /home/mario/GardenStore/backend
 npm run start &
 
 printf "\n--Start Server Frontend\n"
-cd home/mario/GardenStore/frontend
+cd /home/mario/GardenStore/frontend
 npm run start &
 
 printf '\n--GardenStore is up.\n'
