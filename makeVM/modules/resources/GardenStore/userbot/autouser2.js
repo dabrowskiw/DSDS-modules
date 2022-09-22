42178f247c6a7564dc63f4895c961b05292a1230
Robert <59532700+simontree@users.noreply.github.com>
2022-09-22 16:18:58 +0200

    added autouser1 code

--- makeVM/modules/resources/GardenStore/userbot/autouser2.js
+++ makeVM/modules/resources/GardenStore/userbot/autouser2.js
@@ -3,9 +3,26 @@ const puppeteer = require('puppeteer');
 var user = "dolor.vitae@outlook.edu";
 var password = "pw";
 
-(async () => {
-  const browser = await puppeteer.launch();
-  //const browser = await puppeteer.launch();
+var productsArray = [];
+var resultArray = [];
+
+async function getProducts() {
+  fetch(`http://localhost:8000/products`, {
+    method: "GET",
+    credentials: "include",
+  }).then((res) => res.json())
+    .then(
+      (result) => {
+        productsArray = result
+        productsArray.map((product) => resultArray.push("http://localhost:3000/detailPage/" + product.product_id))
+      }
+    );
+}
+getProducts()
+
+async function userbot() {
+  const browser = await puppeteer.launch({args: ['--no-sandbox'], headless: false});
+  // const browser = await puppeteer.launch();
   const page = await browser.newPage();
   //await page.goto('https://www.google.com');
 
@@ -23,6 +40,14 @@ var password = "pw";
   await page.waitForNavigation();
 
   console.log("Logged In");
-  await page.screenshot({ path: 'example.png' });
-  await browser.close();
-})();
+  // await page.screenshot({ path: 'example.png' });
+
+  while (true){
+    for (let i = 0; i < resultArray.length; i++) {
+      await page.goto(resultArray[i]);
+      await new Promise(r => setTimeout(r, 4000));
+    }
+  }
+  // await browser.close();
+};
+userbot();
